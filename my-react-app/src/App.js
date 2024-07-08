f54944dafa662dc435f6990bc467a07968e4950c
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-08 17:36:17 +0900

    add ログイン画面の追加
    ログイン部分のコンポーネントを追加し、ログイン部分を追加しました。

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import Register from './components/Register';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
     return (
         <div>
             <h1>ブログプラットフォーム</h1>
             <Register />
+            <LoginForm />
         </div>
     );
 };
