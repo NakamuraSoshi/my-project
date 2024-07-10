c19c6ca31f211ad6a2556d5c03e9f54c68a94975
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-10 17:46:48 +0900

    add corsを追加
    異なるポートでも接続できるようになった

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,12 +6,14 @@ const userRoutes = require('./routes/userRoutes');
 
 //Expressアプリオブジェを作成
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 //Expressアプリで受信したjson形式のリクエストボディはreq.bodyへ格納
 app.use(bodyParser.json());
 app.use(cors());
 //パスに対してuserRoutesで定義したルーティングを使用する設定
 app.use('/api/users', userRoutes);
 //アプリを指定したポートで起動
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log('Server running on port 3001');
 });
