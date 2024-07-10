faf41cb5c0380cc6da261f9a69b18df1410d234e
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-10 17:38:27 +0900

    add 入力バリデーションを追加

--- my-react-app/src/components/RegisterForm.js
+++ my-react-app/src/components/RegisterForm.js
@@ -8,9 +8,30 @@ const RegisterForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState({});
 
+  const validate = () => {
+    const newErrors = {};
+    const alphanumeric = /^[a-zA-Z0-9]+$/;
+
+    if (!userId) newErrors.userId = 'ユーザーIDは必須です';
+    else if (!alphanumeric.test(userId)) newErrors.userId = 'ユーザーIDは英数字のみ入力可能です';
+
+    if (!username) newErrors.username = 'ユーザー名は必須です';
+
+    if (!password) newErrors.password = 'パスワードは必須です';
+    else if (!alphanumeric.test(password)) newErrors.password = 'パスワードは英数字のみ入力可能です';
+    else if (password.length < 8) newErrors.password = 'パスワードは8文字以上必要です';
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  //handleSubmit関数：フォーム送信で実行される、axioでPOSTリクエストを送信
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (!validate()) return;
+
     try {
       const response = await axios.post('http://localhost:3001/api/users/register', {
         userId,
@@ -32,18 +53,21 @@ const RegisterForm = () => {
           type="text"
           value={userId}
           onChange={(e) => setUserId(e.target.value)}
+          error={errors.userId}
         />
         <InputField
           label="ユーザー名:"
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          error={errors.username}
         />
         <InputField
           label="パスワード:"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          error={errors.password}
         />
         <SubmitButton text="登録" />
       </form>
