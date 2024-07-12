59cbb4c5d5b18cdc762a3a1eb1f1556db2aa4236
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-12 18:03:57 +0900

    add ルートの追加

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import Start from './components/Start';
+import UserDeleteForm from './components/UserDeleteForm';
+import LogoutForm from './components/LogoutForm';
+import { SidebarProvider } from './contexts/SidebarContext';
+import Snackbar from './components/Snackbar';
+import { AuthProvider } from './contexts/AuthContext';
+
+
+const App = () => {
+    const [message, setMessage] = useState('');
+    const [messageType, setMessageType] = useState('');
+    const [redirectTo, setRedirectTo] = useState('');
+
+    //メッセージの内容を設定
+    const showMessage = (msg, type, redirect) => {
+        setMessage(msg);
+        setMessageType(type);
+        setRedirectTo(redirect);
+    };
+
+    const handleCloseSnackbar = () => {
+        setMessage('');
+        setRedirectTo('');
+    };
+
+    //サイドバーの状態共有、ルート指定
+    return (
+        <AuthProvider> {/* AuthProviderでラップ */}
+            <SidebarProvider>
+                <Router>
+                    <Routes>
+                        <Route path='/' element={<Start showMessage={showMessage} />}>
+                            <Route path='login' element={<LoginForm showMessage={showMessage} />} />
+                            <Route path='register' element={<RegisterForm showMessage={showMessage} />} />
+                            <Route path='logout' element={<LogoutForm showMessage={showMessage} />} />
+                            <Route path='delete' element={<UserDeleteForm showMessage={showMessage} />} />
+                        </Route>
+                    </Routes>
+                </Router>
+                {message && (
+                    <Snackbar
+                        message={message}
+                        type={messageType}
+                        onClose={handleCloseSnackbar}
+                        redirectTo={redirectTo}
+                    />
+                )}
+            </SidebarProvider>
+        </AuthProvider>
+    );
+};
+
+
+export default App;
+
+
