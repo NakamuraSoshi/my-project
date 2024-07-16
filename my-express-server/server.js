75a1276c13caed365ea9a9a53dd7a1f5d62d639a
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-16 17:47:54 +0900

    add postsルートの追加

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,8 +1,9 @@
-//expressアプリのルーティングとボディパーサー
+//expressアプリのルーティングとボディパーサーでinputの値を受け取り
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
 
 //Expressアプリオブジェを作成
 const app = express();
@@ -11,8 +12,11 @@ const PORT = process.env.PORT || 3001;
 //Expressアプリで受信したjson形式のリクエストボディはreq.bodyへ格納
 app.use(bodyParser.json());
 app.use(cors());
+
 //パスに対してuserRoutesで定義したルーティングを使用する設定
 app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+
 //アプリを指定したポートで起動
 app.listen(PORT, () => {
     console.log('Server running on port 3001');
