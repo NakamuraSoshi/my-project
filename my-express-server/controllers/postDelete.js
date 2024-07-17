f21d1af3fdd85a13a3f79eec51b64541334c788d
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-17 17:40:33 +0900

    add Post.deleteを呼び、DBから削除する

---
+++ my-express-server/controllers/postDelete.js
@@ -0,0 +1,27 @@
+const Post = require('../models/Post');
+
+const deletePost = (req, res) => {
+  const { userId } = req.user;
+  const { postId } = req.params;
+
+  if (!postId) {
+    return res.status(400).json({ message: '投稿IDが見つかりません' });
+  }
+
+  Post.delete(postId, userId, (error, affectedRows) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ message: '削除に失敗しました' });
+    }
+
+    if (affectedRows > 0) {
+      res.status(200).json({ message: '投稿を削除しました' });
+    } else {
+      res.status(404).json({ message: '投稿が見つかりません' });
+    }
+  });
+};
+
+module.exports = {
+  deletePost,
+};
