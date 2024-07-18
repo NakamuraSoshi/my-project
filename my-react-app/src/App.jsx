c51def7ae7ce7debb95750fd545882fbd86cebd4
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-18 17:42:46 +0900

    add ルートとプロバイダの追加
    マイページのルートを追加、プロバイダーでラップしてコンテキストの適応範囲を決定

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -8,8 +8,11 @@ import LogoutForm from './components/LogoutForm';
 import { SidebarProvider } from './contexts/SidebarContext';
 import Snackbar from './components/Snackbar';
 import { AuthProvider } from './contexts/AuthContext';
-import NewPost from './components/NewPost';
 
+
+import NewPost from './components/NewPost';
+import MyPage from './components/MyPage'; 
+import { UserProvider } from './contexts/UserContext';
 
 const App = () => {
     const [message, setMessage] = useState('');
@@ -30,33 +33,36 @@ const App = () => {
 
     //サイドバーの状態共有、ルート指定
     return (
-        <AuthProvider> {/* AuthProviderでラップ */}
-            <SidebarProvider>
-                <Router>
-                    <Routes>
-                        <Route path='/' element={<Start showMessage={showMessage} />}>
-                            <Route path='login' element={<LoginForm showMessage={showMessage} />} />
-                            <Route path='register' element={<RegisterForm showMessage={showMessage} />} />
-                            <Route path='logout' element={<LogoutForm showMessage={showMessage} />} />
-                            <Route path='delete' element={<UserDeleteForm showMessage={showMessage} />} />
-                            <Route path='create' element={<NewPost showMessage={showMessage} />} />
-                        </Route>
-                    </Routes>
-                </Router>
-                {message && (
-                    <Snackbar
-                        message={message}
-                        type={messageType}
-                        onClose={handleCloseSnackbar}
-                        redirectTo={redirectTo}
-                    />
-                )}
-            </SidebarProvider>
+        <AuthProvider>
+            <UserProvider>
+                <SidebarProvider>
+                    <Router>
+                        <Routes>
+                            <Route path='/' element={<Start showMessage={showMessage} />}>
+                                <Route path='login' element={<LoginForm showMessage={showMessage} />} />
+                                <Route path='register' element={<RegisterForm showMessage={showMessage} />} />
+                                <Route path='logout' element={<LogoutForm showMessage={showMessage} />} />
+                                <Route path='delete' element={<UserDeleteForm showMessage={showMessage} />} />
+                                <Route path='create' element={<NewPost showMessage={showMessage} />} />
+                                <Route path='mypage' element={<MyPage />} /> 
+                            </Route>
+                        </Routes>
+                    </Router>
+                    {message && (
+                        <Snackbar
+                            message={message}
+                            type={messageType}
+                            onClose={handleCloseSnackbar}
+                            redirectTo={redirectTo}
+                        />
+                    )}
+                </SidebarProvider>
+            </UserProvider>
         </AuthProvider>
     );
 };
 
-
 export default App;
+
 
 
