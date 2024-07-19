454427b5ab948ea4b5e9868c2ac3bf97e26cf92f
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-19 17:37:58 +0900

    add ルートの追加

--- my-express-server/routes/postRoutes.js
+++ my-express-server/routes/postRoutes.js
@@ -11,10 +11,12 @@ const { createPost } = require('../controllers/postCreate');
 const { deletePost } = require('../controllers/postDelete'); 
 const verifyToken = require('../middleware/authJwt.js');
 const { myPost } = require('../controllers/mypostDisplay');
+const { allPosts } = require('../controllers/allpostDisplay');
 
 //ルーティングを設定、パスにリクエストが送信されると関数を実行
 router.post('/create',verifyToken, createPost);
 router.delete('/delete',verifyToken, deletePost);
 router.get('/mypost',verifyToken, myPost);
+router.get('/all', allPosts);
 
 module.exports = router;
