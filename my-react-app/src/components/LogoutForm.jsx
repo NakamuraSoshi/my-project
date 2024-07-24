5a91483783520f0bf359d9e474fe7ddc35a7af36
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-07-24 17:44:35 +0900

    add スナックバーの追加

--- my-react-app/src/components/LogoutForm.jsx
+++ my-react-app/src/components/LogoutForm.jsx
@@ -1,57 +1,84 @@
 import React, { useContext, useState } from 'react';
 import axios from 'axios';
-import SubmitButton from './SubmitButton';
+import { Container, Typography, Button, Snackbar, Alert } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthContext';
+import { UserContext } from '../contexts/UserContext';
 
 const LogoutForm = () => {
   const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState('');
   const navigate = useNavigate();
   const { setIsLoggedIn } = useContext(AuthContext);
+  const { setUser } = useContext(UserContext);
 
   const handleLogout = async (event) => {
     event.preventDefault(); 
     try {
-      //ローカルストレージからトークン取得
       const token = localStorage.getItem('token'); 
       if(!token) {
-        console.error('トークンが存在しません');//確認用
+        console.error('トークンが存在しません');
         return;
       }
-      console.log('送信トークン:', token);//確認用
-      //取得したトークンを使用し、サーバーにログアウトリクエスト
+      console.log('送信トークン:', token);
       const response = await axios.post(
         'http://localhost:3001/api/users/logout',
         {},
         {
-          //ヘッダーにトークンを設定し、Bearerスキームで送信
           headers: {
             'Authorization': `Bearer ${token}`
           }
         }
       );
-      //ログアウトできるとトークン削除
       setMessage(response.data.message); 
+      setMessageType('success');
       localStorage.removeItem('token'); 
       setIsLoggedIn(false);
-      console.log('ログアウトしました'); //確認用
+      console.log('ログアウトしました');
+      setUser(null);
       navigate('/');
     } catch (error) {
       console.error('ログアウト時にエラーが発生しました', error);
+      setMessage('ログアウト時にエラーが発生しました');
+      setMessageType('error');
       localStorage.removeItem('token'); 
       setIsLoggedIn(false);
       window.location.href = '/';
     }
   };
 
+  const handleCloseSnackbar = () => {
+    setMessage('');
+  }
+
   return (
-    <div>
-      <h2>ログアウト</h2>
-      {message && <p>{message}</p>}
+    <Container maxWidth="xs">
+      <Typography variant="h4" component="h1" gutterBottom>
+        ログアウト
+      </Typography>
+      {message && (
+        <Snackbar
+          open={!!message}
+          autoHideDuration={6000}
+          onClose={handleCloseSnackbar}
+        >
+          <Alert onClose={handleCloseSnackbar} severity={messageType} sx={{ width: '100%' }}>
+            {message}
+          </Alert>
+        </Snackbar>
+      )}
       <form onSubmit={handleLogout}>
-        <SubmitButton text="ログアウト" />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          style={{ marginTop: '16px' }}
+        >
+          ログアウト
+        </Button>
       </form>
-    </div>
+    </Container>
   );
 };
 
