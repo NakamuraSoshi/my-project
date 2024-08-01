d213817f2d850b315ce5f1885ad164e89ce36448
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-08-01 17:38:07 +0900

    add 切断処理を追加

--- my-express-server/config/database.js
+++ my-express-server/config/database.js
@@ -16,5 +16,19 @@ db.connect(err =>{
   console.log('Connected');
 });
 
+//クローズ処理
+const closeConnection = () => {
+  db.end(err => {
+    if (err) {
+      console.error('Error', err);
+    } else {
+      console.log('接続を終了しました');
+    }
+  });
+};
+
 //モジュールとしてエクスポートすると他ファイルでインポートして接続できる
-module.exports = db;
+module.exports = { 
+  db, 
+  closeConnection 
+};
