fa298a63756e5bd0ca5ec8344475fe78d09ecb3e
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-08-06 09:13:19 +0900

    change ログイン状態の判断法の変更
    ローカルストレージのトークンの有無から、トークンの有効性をバックで確認に変更

--- my-react-app/src/contexts/AuthContext.jsx
+++ my-react-app/src/contexts/AuthContext.jsx
@@ -1,25 +1,49 @@
 import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import BaseURL from '../config/url'; // BaseURLをインポート
 
-//ログインの認証を管理、共有
+// ログインの認証を管理、共有
 export const AuthContext = createContext();
 
-//共有するためのコンテキストオブジェクト、useStateで状態変数を定義
+// 共有するためのコンテキストオブジェクト、useStateで状態変数を定義
 export const AuthProvider = ({ children }) => {
+    //認証状態を管理
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    //実行される内容
     useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsLoggedIn(true);
-        } else {
-            setIsLoggedIn(false);
-        }
+        const checkToken = async () => {
+            try {
+                //クッキーからトークン抽出
+                const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, "$1");
+                if (token) {
+                    // トークンの有効性をサーバーで確認
+                    const response = await axios.post(`${BaseURL}/users/tokenCheck`, {}, {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    });
+                    //有効
+                    if (response.status === "成功") {
+                        setIsLoggedIn(true);
+                    } else {
+                        //無効
+                        setIsLoggedIn(false);
+                    }
+                } else {
+                    //存在しない
+                    setIsLoggedIn(false);
+                }
+            } catch (error) {
+                //エラー
+                setIsLoggedIn(false);
+            }
+        };
+
+        checkToken();
     }, []);
 
-    //AuthContext.ProviderでisLoggedInの値を子に渡す
     return (
-        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn}}>
+        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
             {children}
         </AuthContext.Provider>
     );
