c3b9f413a8e103e51d5c2909f4672334bc392276
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-08-06 09:10:56 +0900

    add エラータイプの定数

---
+++ my-react-app/src/config/constants.js
@@ -0,0 +1,12 @@
+export const MESSAGE_TYPES = {
+  SUCCESS: 'success',
+  ERROR: 'error',
+};
+
+export const COLORS = {
+  PRIMARY: 'primary',
+};
+
+export const SNACKBAR_SETTINGS = {
+  AUTO_HIDE_DURATION: 6000,
+};
