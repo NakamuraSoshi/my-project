70108d533fc7ebe0532c2003e511ed89847b9ff5
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-08-06 09:28:40 +0900

    change メッセージ表示と処理変更
    エラー時のメッセージ表示を追加、退会処理時にuserをnullに変更、エラー時にトークン削除からメッセージ表示に変更

--- my-react-app/src/components/UserDeleteForm.jsx
+++ my-react-app/src/components/UserDeleteForm.jsx
@@ -1,53 +1,49 @@
 import React, { useContext, useState } from 'react';
 import axios from 'axios';
-import SubmitButton from './SubmitButton';
 import { AuthContext } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
-
+import { UserContext } from '../contexts/UserContext';
+import BaseURL from '../config/url';
 
 const UserDeleteForm = () => {
   const [message, setMessage] = useState('');
   const { setIsLoggedIn } = useContext(AuthContext);
+  const { setUser } = useContext(UserContext);
   const navigate = useNavigate();
+  //送信中の状態を追加
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleDelete = async (event) => {
     event.preventDefault();
+    setIsSubmitting(true);
 
-    try{
-      const token = localStorage.getItem('token');
-      if(!token) {
-        console.error('トークンがありません');
-        return;
-      }
-
-      const response = await axios.post(
-        'http://localhost:3001/api/users/delete',
-        {},
+    try {
+      const response = await axios.delete(
+        `${BaseURL}/users/delete`,
         {
-          headers: {
-            'Authorization': `Bearer ${token}` 
-          }
+          withCredentials: true // クッキーを含めてリクエストを送信
         }
       );
 
       setMessage(response.data.message);
-      localStorage.removeItem('token');
       setIsLoggedIn(false);
+      setUser(null);
       navigate('/');
     } catch (error) {
-      console.error('退会処理中にエラーが発生しました', error);
-      localStorage.removeItem('token'); 
-      setIsLoggedIn(false);
-      navigate('/');
+      setMessage('退会処理中にエラーが発生しました。再度お試しください');
+    }finally {
+      setIsSubmitting(false);
     }
   };
 
   return (
     <div>
       <h2>退会</h2>
-      {message && <p>{message}</p>} {/* メッセージがあれば表示 */}
+      {message && <p>{message}</p>} 
       <form onSubmit={handleDelete}>
-        <SubmitButton text="退会する" />
+      <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? '処理中...' : '退会する'}
+        </button>
       </form>
     </div>
   );
