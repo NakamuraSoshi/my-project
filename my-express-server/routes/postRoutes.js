d65a2e80acb2c755529a20bd8b6c86dba33bbcea
NakamuraSoshi <soshi.nakamura@excite-software.co.jp>
2024-08-06 09:21:06 +0900

    change ルートの変更

--- my-express-server/routes/postRoutes.js
+++ my-express-server/routes/postRoutes.js
@@ -7,18 +7,18 @@ const express = require('express');
 const router = express.Router();
 
 //requireでそれぞれのコントローラー関数を読み込み
-const { createPost } = require('../controllers/postCreate');
-const { deletePost } = require('../controllers/postDelete'); 
+const { createPost } = require('../controllers/post/postCreate.js');
+const { deletePost } = require('../controllers/post/postDelete.js'); 
 const verifyToken = require('../middleware/authJwt.js');
-const { myPost } = require('../controllers/mypostDisplay');
-const { allPosts } = require('../controllers/allpostDisplay');
-const { searchPosts } = require('../controllers/postSearch');
+const { myPost } = require('../controllers/post/postMyPage.js');
+const { allPosts } = require('../controllers/post/postHomePage.js');
+const { searchPosts } = require('../controllers/post/postSearch.js');
 
 //ルーティングを設定、パスにリクエストが送信されると関数を実行
-router.post('/create',verifyToken, createPost);
+router.put('/create',verifyToken, createPost);
 router.delete('/delete',verifyToken, deletePost);
 router.get('/mypost',verifyToken, myPost);
 router.get('/all', allPosts);
-router.get('/search', searchPosts);
+router.get('/search', verifyToken, searchPosts);
 
 module.exports = router;
--- my-express-server/routes/userRoutes.js
+++ my-express-server/routes/userRoutes.js
@@ -7,18 +7,20 @@ const express = require('express');
 const router = express.Router();
 
 //requireでそれぞれのコントローラー関数を読み込み
-const { registerUser } = require('../controllers/userRegister');
-const { loginUser } = require('../controllers/userLogin');
-const { logoutUser } = require('../controllers/userLogout');
-const verifyToken = require('../middleware/authJwt.js');
-const { deleteUser } = require('../controllers/userDelete');
-const { infoUser } = require('../controllers/userInfo.js');
+const { registerUser } = require('../controllers/user/userRegister.js');
+const { loginUser } = require('../controllers/user/userLogin.js');
+const { logoutUser } = require('../controllers/user/userLogout.js');
+const verifyToken  = require('../middleware/authJwt.js');
+const { deleteUser } = require('../controllers/user/userDelete.js');
+const { infoUser } = require('../controllers/user/userInfo.js');
+const { checkUser } = require('../controllers/user/userCheck');
 
 //ルーティングを設定、パスにリクエストが送信されると関数を実行
-router.post('/register', registerUser);
+router.put('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', verifyToken, logoutUser);
-router.post('/delete', verifyToken, deleteUser);
+router.delete('/delete', verifyToken, deleteUser);
 router.get('/info', verifyToken, infoUser);
+router.checkout('/check', verifyToken, checkUser);
 
 module.exports = router;
